2f0a8b230313d792285b5f4b1fd05d922ea52e67
Rabia <sdetcode@gmail.com>
2022-06-08 01:34:00 +0530

    ratings data service

---
+++ src/main/java/com/spring/boot/ratingsdataservice/RatingsResource.java
@@ -0,0 +1,27 @@
+package com.spring.boot.ratingsdataservice;
+
+import com.spring.boot.ratingsdataservice.models.Rating;
+import com.spring.boot.ratingsdataservice.models.UserRating;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingsResource {
+
+
+    @RequestMapping("/movies/{movieId}")
+    public Rating getMovieRating(@PathVariable("movieId") String movieId) {
+        return new Rating(movieId, 4);
+    }
+
+    @RequestMapping("/user/{userId}")
+    public UserRating getUserRatings(@PathVariable("userId") String userId) {
+        UserRating userRating = new UserRating();
+        userRating.initData(userId);
+        return userRating;
+
+    }
+
+}
